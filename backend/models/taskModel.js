2c6e6bbf01eb4d2b06bbf95cd0f1082ad8a11038
truong180805 <nguyentruong180805@gmail.com>
2025-12-23 22:47:35 +0700

    Add task model schema with fields for project, title, description, assignedTo, dueDate, status, submissionLink, and lecturerComment

---
+++ backend/models/taskModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        required: true
+    },
+    title: { type: String, rquired: true},
+    descsription: { type: String },
+
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    ueDate: { type: Date },
+  
+  status: { 
+    type: String, 
+    enum: ['todo', 'in_progress', 'submitted', 'completed'], 
+    default: 'todo' 
+  },
+  
+  submissionLink: { type: String },
+  lecturerComment: { type: String }
+}, {timestamps: true}
+); module.exports = mongoose.model('Task', taskSchema);
