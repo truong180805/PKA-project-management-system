64d921d5b3c0fc36d70653544f9c577efc67ea26
truong180805 <nguyentruong180805@gmail.com>
2025-12-24 08:48:07 +0700

    Add login functionality to userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcryjpt = requir('bcryjpt');
 const User = requir('../models/userModel');
 
-//fuction register
+//register function
 const registerUser = async (req, res) => {
     try{
         const{ fullName, email, password, role, university, studenId, classParams, department } = req.body;
@@ -41,3 +41,37 @@ const registerUser = async (req, res) => {
     }
 };
 
+// login fuction
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user && (await bcryjpt.compare(password, user.password))) {
+            res.json({
+                _id: user.id,
+                fullName: user.fullName,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(400).json({ message: 'Email hoặc mật khẩu không đúng'});
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message});
+    }
+};
+
+// create token fuction
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SCRET, {
+        expiresIn: '30d',
+    });
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
