ccc1da76c593ba8926ba8ccb246ddcf50854ba83
truong180805 <nguyentruong180805@gmail.com>
2025-12-25 12:04:08 +0700

    Fix member property name in createProject function and add getProjects and findProjectById functions

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -21,7 +21,7 @@ const createProject = async (req, res) => {
             projectData.isStudentProposed = true;
             projectData.status = 'pending_approval';
             projectData.leader = currentUser._id;
-            projectData.member = [currentUser._id];
+            projectData.members = [currentUser._id];
             projectData.mentor = mentorId;
         }
         else if(currentUser.role === 'lecturer'){
@@ -42,4 +42,40 @@ const createProject = async (req, res) => {
     } catch (error) {
         res.status(400).json({message: 'Tạo đề tài thất bại', error: error.message});
     }
-}
+};
+
+const getProjects = async (req, res) => {
+    try{
+
+        const projects = await Project.find()
+        .populate('leader', 'fullName email studentId')
+        .populate('mentor', 'fullName email');
+
+        res.json(projects);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+const findProjectById = async (req, res) => {
+    try{
+        const project = await Project.findById(req.params.id)
+        .populate('members', 'fullName email studentId')
+        .populate('leader', 'fullName email studentId')
+        .populate('mentor', 'fullName email');
+
+        if(project){
+            res.json(project);
+        }else{
+            res.status(404).json({message: 'không tìm thấy đề tài'});
+        }
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+module.exports = {
+    createProject,
+    getProjects,
+    findProjectById
+};
