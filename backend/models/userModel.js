08c67e58d396cc404a6efe9255e754f90bce335d
truong180805 <nguyentruong180805@gmail.com>
2025-12-31 07:53:46 +0700

    refactor: reorganize user schema fields and ensure required properties

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,33 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    // login
+    numberPhone: { type: String, required: true, unique: true},
     fullName: { type: String, required: true},
-    email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
+    
+    //later update in profile
+    email: { type: String, required: true, unique: true},
     university: { type: String, required: true, default: 'Phenikaa'},
+    gender: { type: String, enum: ['Nam', 'Nữ', 'Khác']},
+    dateOfBirth: { type: Date},
+    avatarUrl: { type: String, default: "" },
 
     role: {
         type: String,
         enum: ['admin', 'lecturer', 'student'], 
-        default: 'student'
+        required: true
     },
 
     //Student
     studentId: { type: String },
-    classParmas: { type: String },
+    className: { type: String },
+    major: {type: String},
 
     //lecturer
     department: { type: String },
 
-    avatarUrl: { type: String, default: "" }
+    isActive: { type: Boolean, default: true }
 }, { timestamps: true});
 
 module.exports = mongoose.model('User', userSchema);
