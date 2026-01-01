33611a852b6ba8f77a4c153b59e3044024aee401
truong180805 <nguyentruong180805@gmail.com>
2026-01-01 22:37:27 +0700

    feat: enhance getMyClasses function to support student role and add joinClass functionality with auto-approval

--- backend/controllers/classController.js
+++ backend/controllers/classController.js
@@ -39,8 +39,17 @@ const createClass = async (req, res) => {
 
 const getMyClasses = async (req, res) => {
     try{
-        const classes = await Class.find({ lecturer: req.user._id})
-        .sort({ createdAt: -1});
+        let classes;
+        
+        if (req.user.role === 'lecturer') {
+            classes = await Class 
+                .find({ lecturer: req.user._id })
+                .sort({ createdAt: -1 });
+        } else if (req.user.role === 'student') {
+            classes = await Class
+                .find({ student: req.user._id })
+                .sort({ createdAt: -1 });
+        }
 
         res.json(classes);
     } catch (error) {
@@ -48,7 +57,45 @@ const getMyClasses = async (req, res) => {
     }
 };
 
+const joinClass = async (req, res) => {
+    try{
+        const { classCode } = req.body;
+        const studentId = req.user._id;
+
+        const classItem = await Class.findOne({ classCode });
+        if (!classItem) {
+            return res.status(404).json({ message: 'Lớp không tồn tại'});
+        }
+        const isJoined = classItem.student.some(
+            id => id.equals(studentId)
+        );
+        const isPending = classItem.student.some(
+            id => id.equals(studentId)
+        );
+
+        if (isJoined){
+            return res.status(400).json({ message: 'Bạn đã là thành viên của lớp'});
+        }
+        if (isPending){
+            return res.status(400).json({ message: 'Bạn đã gửi yêu cầu, vui lòng chờ'});
+        }
+
+        if (classItem.settings.autoApprove) {
+            classItem.students.push(studentId);
+            await classItem.save();
+            return res.status(200).json({ message: 'Tham gia lớp thành công', status: 'joined'});
+        } else {
+            classItem.pendingStudents.push(studentId);
+            await classItem.save();
+            return res.status(200).json({ message: 'Đã gửi yêu cầu tham gia, chờ duyệt', status: 'pending'});
+        }
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports ={
     createClass,
-    getMyClasses
+    getMyClasses,
+    joinClass
 };
