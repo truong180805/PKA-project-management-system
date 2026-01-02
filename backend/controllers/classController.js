19c147134219f0f65af9f5bf5b3e12e204ee886a
truong180805 <nguyentruong180805@gmail.com>
2026-01-02 13:37:51 +0700

    feat: add approveStudent function to manage student approvals in classes

--- backend/controllers/classController.js
+++ backend/controllers/classController.js
@@ -120,9 +120,51 @@ const getClassDetails = async (req, res) =>{
     }
 };
 
+const approveStudent = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {studentId, isApproved} = req.body;
+
+        const classItem = await Class.findById(id);
+
+        if(!classItem){
+            return res.status(404).json({ message: 'Lớp không tồn tại'});
+        }
+
+        const isPending = classItem.pendingStudents.some(
+        id => id.equal(studentId)
+        );
+
+        if(classItem.lecturer.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: 'Chỉ giảng viên mới được duyệt'});
+        }
+
+        if(!isPending) {
+            return res.status(400).json({ message: 'Sinh viên này không nằm trong danh sách chờ'});
+        }
+
+        if (isApproved) {
+        classItem.pendingStudents = classItem.pendingStudents.filter(
+            id => id.toString() !== studentId
+        );
+        classItem.student.push(studentId);
+        } else {
+        classItem.pendingStudents = classItem.pendingStudents.filter(
+            id => id.toString() !== studentId
+        );
+        }
+
+        await classItem.save();
+        res.json({ message: isApproved ? 'Đã duyệt sinh viên' : 'Đã từ chối'});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports ={
     createClass,
     getMyClasses,
     joinClass,
-    getClassDetails
+    getClassDetails,
+    approveStudent
 };
