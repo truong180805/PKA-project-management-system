1778d93a65731ed30d739d32f50a65623d518e45
truong180805 <nguyentruong180805@gmail.com>
2026-01-07 09:13:18 +0700

    import ClassPage in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage'
 import MainLayout from './components/MainLayout';
+import ClassPage from './pages/ClassPage';
 
 const DashboardHome = () => <h2>He thong quan ly</h2>;
+const ProjectPage = () => <h2>Trang do an</h2>;
 
 function App() {
   return(
@@ -15,9 +17,9 @@ function App() {
         <Route index element={<Navigate to="dashboard"/>} />
         
         <Route path="dashboard" element={<DashboardHome/>} />
-        <Route path="classes" element={<h2>Trang Quản lý Lớp</h2>} />
-        <Route path="projects" element={<h2>Trang Đồ án</h2>} />
-        <Route path="users" element={<h2>Trang người dùng</h2>} />
+        <Route path="classes" element={<ClassPage/>} />
+        <Route path="projects" element={<ProjectPage/>} />
+        <Route path="users" element={<h2>Trang nguoi dung</h2>} />
       </Route>
     </Routes>
   );
---
+++ frontend/src/pages/ClassPage.jsx
@@ -0,0 +1,5 @@
+const ClassPage = () => {
+    return <h1>trang lop</h1>
+}
+
+export default ClassPage;
